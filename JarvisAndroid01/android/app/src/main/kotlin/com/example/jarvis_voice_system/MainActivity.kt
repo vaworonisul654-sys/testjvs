4650d3f031c62de769b4a9df6c753d8484e86122
asheis <spacexman.01@icloud.com>
2026-03-03 16:44:26 +0700

    feat: initialize native android/ios folders and configure permissions

---
+++ JarvisAndroid01/android/app/src/main/kotlin/com/example/jarvis_voice_system/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jarvis_voice_system
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ JarvisAndroid01/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audioplayers_darwin
+import record_darwin
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
